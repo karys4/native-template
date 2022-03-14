5983617f6476276d3015ce16e45c3a2a0e1fcecb
karyrodriguez <kary.github@gmail.com>
2022-03-13 20:44:37 -0600

    Validar campos vacios en el formulario y mostrar un alert

--- views/loginForm/LoginForm.js
+++ views/loginForm/LoginForm.js
@@ -1,4 +1,4 @@
-import React from "react";
+//import React from "react";
 import {
   StyleSheet,
   View,
@@ -8,53 +8,110 @@ import {
   TouchableOpacity,
   Linking,
   Image,
+  Alert,
 } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import React, { Component } from "react";
+import { color } from "react-native/Libraries/Components/View/ReactNativeStyleAttributes";
 
-export default function LoginForm({ text }) {
-  return (
-    //Si es más de un elemento, debe ir dentro de un View.
-    <SafeAreaView style={styles.principal}>
-      <View style={styles.container}>
-        <Image
-          style={styles.imagen}
-          source={require("../../assets/Logo.png")}
-        />
-        <TextInput
-          style={styles.input}
-          placeholder="Correo Electrónico"
-          autoCapitalize="none"
-          placeholderTextColor={"#6f4a8e"}
-        />
-        <TextInput
-          style={styles.input}
-          placeholder="Contraseña"
-          autoCapitalize="none"
-          secureTextEntry={true}
-          placeholderTextColor={"#6f4a8e"}
-        />
-        <TouchableOpacity style={styles.boton}>
-          <Text style={styles.titleButton}>{text}Iniciar sesión</Text>
-        </TouchableOpacity>
+export default class LoginForm extends Component {
+  constructor() {
+    super();
+    this.state = {
+      correo: "",
+      contrasena: "",
+    };
+  }
+  cambioCampoCorreo(correo) {
+    this.setState({ correo });
+  }
 
-        <Text
-          style={{
-            color: "white",
-            fontSize: 15,
-            margin: 20,
-            textAlign: "center",
-            textDecorationLine: "underline",
-          }}
-          onPress={() => Linking.openURL("#")}
-        >
-          Olvidaste la contraseña
-        </Text>
-        <TouchableOpacity style={styles.boton}>
-          <Text style={styles.titleButton}>{text}Registrarse</Text>
-        </TouchableOpacity>
-      </View>
-    </SafeAreaView>
-  );
+  cambioCampoContrasena(contrasena) {
+    this.setState({ contrasena });
+  }
+
+  cambioTextoCampos(textoCampos) {
+    this.setState({ textoCampos });
+  }
+
+  botonOprimido() {
+    if (this.state.correo == "" && this.state.contrasena != "") {
+      alert(this.state.correo + "Introduce tu correo electrónico.");
+    } else if (this.state.contrasena == "" && this.state.correo != "") {
+      alert(this.state.contrasena + "Introduce tu contraseña.");
+    } else if ((this.state.correo && this.state.contrasena) == "") {
+      alert(
+        this.state.correo +
+          this.state.contrasena +
+          "Introduce tu correo electrónico y contraseña."
+      );
+    }
+  }
+
+  render() {
+    return (
+      //Si es más de un elemento, debe ir dentro de un View.
+      <SafeAreaView style={styles.principal}>
+        <View style={styles.container}>
+          <Image
+            style={styles.imagen}
+            source={require("../../assets/Logo.png")}
+          />
+          <TextInput
+            style={styles.input}
+            placeholder="Correo Electrónico"
+            autoCapitalize="none"
+            placeholderTextColor={"#6f4a8e"}
+            value={this.state.correo}
+            onChangeText={(correo) => this.cambioCampoCorreo(correo)}
+          />
+          <Text
+            style={styles.textoCampos}
+            value={this.state.textoCampos}
+            onChangeText={(textoCampos) => this.cambioTextoCampos(textoCampos)}
+          >
+            Hola
+          </Text>
+
+          <TextInput
+            style={styles.input}
+            placeholder="Contraseña"
+            autoCapitalize="none"
+            secureTextEntry={true}
+            placeholderTextColor={"#6f4a8e"}
+            value={this.state.contrasena}
+            onChangeText={(contrasena) =>
+              this.cambioCampoContrasena(contrasena)
+            }
+          />
+          <TouchableOpacity style={styles.boton}>
+            <Text
+              style={styles.titleButton}
+              onPress={() => this.botonOprimido()}
+            >
+              Iniciar sesión
+            </Text>
+          </TouchableOpacity>
+
+          <Text
+            style={{
+              color: "white",
+              fontSize: 15,
+              margin: 20,
+              textAlign: "center",
+              textDecorationLine: "underline",
+            }}
+            onPress={() => Linking.openURL("#")}
+          >
+            Olvidaste la contraseña
+          </Text>
+          <TouchableOpacity style={styles.boton}>
+            <Text style={styles.titleButton}>Registrarse</Text>
+          </TouchableOpacity>
+        </View>
+      </SafeAreaView>
+    );
+  }
 }
 
 //Agregando estilos a los componentes
@@ -94,5 +151,8 @@ const styles = StyleSheet.create({
   imagen: {
     alignContent: "center",
     justifyContent: "center",
+  },
+  textoCampos: {
+    color: "#6f4a8e",
   },
 });
